3185a9c7e3cce305c3c0462e8f8d3ca5eecda532
44jovi <kwong.jovi@gmail.com>
2024-01-08 17:03:21 +0000

    create(CitizenResource): get()

---
+++ src/main/java/com/example/citizen/rest/resource/CitizenResource.java
@@ -0,0 +1,15 @@
+package com.example.citizen.rest.resource;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/register")
+public class CitizenResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String get(){
+        return "Hello - I am a WIP.";
+    }
+}
