13ed702bca59a6ddea15ce7bad44f4c84fe5ad93
44jovi <kwong.jovi@gmail.com>
2024-01-08 17:01:29 +0000

    create(Citizen.java)

---
+++ src/main/java/com/example/citizen/rest/model/Citizen.java
@@ -0,0 +1,53 @@
+package com.example.citizen.rest.model;
+
+import java.time.Instant;
+
+public class Citizen {
+
+    private String givenName;
+    private String familyName;
+    private Instant dob;
+    private String NINumber;
+
+    @Override
+    public String toString() {
+        return "Citizen{" +
+                "givenName='" + givenName + '\'' +
+                ", familyName='" + familyName + '\'' +
+                ", dob=" + dob +
+                ", NINumber='" + NINumber + '\'' +
+                '}';
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public Instant getDob() {
+        return dob;
+    }
+
+    public void setDob(Instant dob) {
+        this.dob = dob;
+    }
+
+    public String getNINumber() {
+        return NINumber;
+    }
+
+    public void setNINumber(String NINumber) {
+        this.NINumber = NINumber;
+    }
+}
