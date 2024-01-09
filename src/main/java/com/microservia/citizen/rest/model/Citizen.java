d8f80655550829cd9d1439eaad643b1260cf6c83
44jovi <kwong.jovi@gmail.com>
2024-01-09 09:25:10 +0000

    reformat

--- src/main/java/com/microservia/citizen/rest/model/Citizen.java
+++ src/main/java/com/microservia/citizen/rest/model/Citizen.java
@@ -4,50 +4,50 @@ import java.time.Instant;
 
 public class Citizen {
 
-    private String givenName;
-    private String familyName;
-    private Instant dob;
-    private String NINumber;
+  private String givenName;
+  private String familyName;
+  private Instant dob;
+  private String NINumber;
 
-    @Override
-    public String toString() {
-        return "Citizen{" +
-                "givenName='" + givenName + '\'' +
-                ", familyName='" + familyName + '\'' +
-                ", dob=" + dob +
-                ", NINumber='" + NINumber + '\'' +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Citizen{" +
+        "givenName='" + givenName + '\'' +
+        ", familyName='" + familyName + '\'' +
+        ", dob=" + dob +
+        ", NINumber='" + NINumber + '\'' +
+        '}';
+  }
 
-    public String getGivenName() {
-        return givenName;
-    }
+  public String getGivenName() {
+    return givenName;
+  }
 
-    public void setGivenName(String givenName) {
-        this.givenName = givenName;
-    }
+  public void setGivenName(String givenName) {
+    this.givenName = givenName;
+  }
 
-    public String getFamilyName() {
-        return familyName;
-    }
+  public String getFamilyName() {
+    return familyName;
+  }
 
-    public void setFamilyName(String familyName) {
-        this.familyName = familyName;
-    }
+  public void setFamilyName(String familyName) {
+    this.familyName = familyName;
+  }
 
-    public Instant getDob() {
-        return dob;
-    }
+  public Instant getDob() {
+    return dob;
+  }
 
-    public void setDob(Instant dob) {
-        this.dob = dob;
-    }
+  public void setDob(Instant dob) {
+    this.dob = dob;
+  }
 
-    public String getNINumber() {
-        return NINumber;
-    }
+  public String getNINumber() {
+    return NINumber;
+  }
 
-    public void setNINumber(String NINumber) {
-        this.NINumber = NINumber;
-    }
+  public void setNINumber(String NINumber) {
+    this.NINumber = NINumber;
+  }
 }
